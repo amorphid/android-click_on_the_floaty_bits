641c34882709b1e17dba1743f757183003daa41d
Michael Pope <mpope.cr@gmail.com>
2015-03-02 11:50:19 -0500

    Make app get random color from Heroku

--- app/src/main/java/com/pope4president/clickonthefloatybits/MainActivity.java
+++ app/src/main/java/com/pope4president/clickonthefloatybits/MainActivity.java
@@ -8,7 +8,14 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
-import java.util.Random;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 
 public class MainActivity extends Activity {
@@ -25,7 +32,17 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
 
         View view = findViewById(R.id.circle);
-        setCircleBackGroundColor(view);
+
+        try {
+            setCircleBackGroundColor(view);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
     }
 
     @Override
@@ -50,19 +67,54 @@ public class MainActivity extends Activity {
         return super.onOptionsItemSelected(item);
     }
 
-    public String randomHexColor() {
-        String color = "#";
+    public String randomHexColor() throws IOException, JSONException, InterruptedException {
+        final String urlString             = "http://rack-random-color.herokuapp.com/";
+        final JSONObject jsonObject        = new JSONObject();
+        URL url                      = new URL(urlString);
+        final HttpURLConnection connection = (HttpURLConnection) url.openConnection();
 
-        for (int i = 0; i < 6; i += 1) {
-            Random random = new Random();
-            int num = random.nextInt(values.length);
-            color += values[num];
-        }
+        Thread thread = new Thread(new Runnable() {
+            public void run() {
+                BufferedReader reader        = null;
+                try {
+                    reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
 
-        return color;
+                StringBuilder response       = new StringBuilder();
+                String inputLine;
+
+                try {
+                    while ((inputLine = reader.readLine()) != null) {
+                        response.append(inputLine);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                try {
+                    jsonObject.put(urlString, response.toString());
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        thread.start();
+        thread.join();
+
+        JSONObject colorJson = new JSONObject((String) jsonObject.get(urlString));
+        return (String) colorJson.get("color");
     }
 
-    public void setCircleBackGroundColor(View view) {
+    public void setCircleBackGroundColor(View view) throws IOException, JSONException, InterruptedException {
         GradientDrawable shape = (GradientDrawable) view.getBackground();
         shape.setColor(Color.parseColor(randomHexColor()));
     }
